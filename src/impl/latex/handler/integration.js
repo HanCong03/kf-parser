129e9342ad91f85dcbff35e4d594d6945d1df78f
hancong03 <hancong03@baidu.com>
2014-07-29 15:12:13 +0800

    fix \int\int bug
    Signed-off-by: hancong03 <hancong03@baidu.com>

--- src/impl/latex/handler/integration.js
+++ src/impl/latex/handler/integration.js
@@ -4,12 +4,19 @@
 
 define( function ( require ) {
 
-    var ScriptExtractor = require( "impl/latex/handler/lib/script-extractor" );
+    var ScriptExtractor = require( "impl/latex/handler/lib/script-extractor" ),
+        FN_TYPE = require( "impl/latex/define/type" ).FN;
 
     return function ( info, processedStack, unprocessedStack ) {
 
         var count = unprocessedStack.shift(),
             params = ScriptExtractor.exec( unprocessedStack );
+
+        if ( params.expr && params.expr.type === FN_TYPE ) {
+
+            params.expr = params.expr.handler( params.expr, processedStack, unprocessedStack );
+
+        }
 
         info.operand = [ params.expr, params.superscript, params.subscript ];
         // 参数配置调用
